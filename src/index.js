28bcbc29d2bb1e070b2ff4c60f3829ffc6194086
ArunRaj <arun@gencomtel.com>
2022-02-18 23:58:46 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom";
+import Comment from "./components/comments/Comment";
+import ApprovalCard from "./components/comments/ApprovalCard";
+const App = ()=>{
+    const obj = {
+        name : "arun",
+        comment : "Hi hello there"
+    };
+    return (
+        <div>
+            <ApprovalCard>
+                <Comment {...obj}/>
+            </ApprovalCard>
+            <ApprovalCard>
+                <Comment {...obj}/>
+            </ApprovalCard>
+        </div>
+    )
+}
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+ReactDom.render(
+    <App />,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
