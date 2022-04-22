a1c75cfc001fed1cab73a1aeba9b4db55f77b240
ArunRaj <arun@gencomtel.com>
2022-04-22 11:54:51 +0530

    main initial commit

--- src/index.js
+++ src/index.js
@@ -1,24 +1,12 @@
 import React from "react";
 import ReactDom from "react-dom";
-import Comment from "./components/comments/Comment";
-import ApprovalCard from "./components/comments/ApprovalCard";
 const App = ()=>{
-    const obj = {
-        name : "arun",
-        comment : "Hi hello there"
-    };
     return (
         <div>
-            <ApprovalCard>
-                <Comment {...obj}/>
-            </ApprovalCard>
-            <ApprovalCard>
-                <Comment {...obj}/>
-            </ApprovalCard>
+            Welcome
         </div>
     )
 }
-
 ReactDom.render(
     <App />,
     document.getElementById('root')
